ff67a50df926fdfd69712a4bb227ccf7522347d9
Erik Mathlener <torongla@xs4all.nl>
2013-06-01 23:55:37 +0200

    Eerste Upload EMF

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emf::Application.config.secret_token = '8e42070a5552795e2d17d6373a8cfb277ab0318083e42dac0b6ebbaf8be97e331dca1023e7d75c9181e85c922055976ad83975b2be05b07ba24b5465ccf0b0e6'
